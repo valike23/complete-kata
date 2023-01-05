0ea7c734bb07c179da1346c2b1f1e752ff0c9742
valike23 <ludike23@gmail.com>
2023-01-05 06:56:02 +0100

    update the code

---
+++ src/routes/api/entries/aux.ts
@@ -0,0 +1,14 @@
+import { Entry, Ientry } from "../../../Controllers/entries";
+
+// update an entry record the club and name 
+export async function put(req, res){
+try {
+    const entry : Ientry= JSON.parse(req.fields.body);
+    console.log(entry);
+    const data = await Entry.update(entry,{where: {id: entry.id}});
+    res.json(data);
+} catch (error) {
+    console.log(error);
+    res.status(503).json(error);
+}
+}
