b64df5943ee7957dade2e6ef456916fbec46eb55
valike23 <ludike23@gmail.com>
2023-03-09 19:53:20 +0100

    updated the topbar

--- src/components/TopBar.svelte
+++ src/components/TopBar.svelte
@@ -1,36 +1,111 @@
 <script>
-  import {goto} from "@sapper/app";
+  import { goto } from "@sapper/app";
 
-
-    const gohome = ()=>{
-        goto('/');
-    }
-    const goback =()=>{
-        history.back();
-    }
+  const nav = (screen) => {
+    goto(screen);
+  };
+  const gohome = () => {
+    goto("/");
+  };
+  const goback = () => {
+    history.back();
+  };
 </script>
 
 <!-- svelte-ignore missing-declaration -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div class="row mt-2" >
+<div class="row mt-2">
   <div class="cell-12">
-    <ul class="t-menu open bar bg-black fg-white horizontal">
-        <!-- svelte-ignore a11y-invalid-attribute -->
-        <li><a on:click={goback} href="#"><span class="mif-arrow-left icon"></span></a></li>
-        <li><a href="/competition"><span class="mif-trophy icon"></span></a></li>
-        <li><a href="/category"><span class="mif-stack icon"></span></a></li>
-        <li><a title="entries" href="/entries"><span class="mif-user icon"></span></a></li>
-        <li><a title="kata" href="/kata"><span class="mif-directions-bike icon"></span></a></li>
-        <li><a title="pools" href="/pools"><span class="mif-apps icon"></span></a></li>
-        <li><a target="_blank" title=" external screens" href="/screen/external"><span class="mif-laptop icon"></span></a></li>
-        <li><a title="pools" href="/competition_controller"><span class="mif-tree icon"></span></a></li>
-        <li><a target="_blank" title="judges screens" href="/screen/judgescreen"><span class="mif-tablet icon"></span></a></li>
-        <li><a  title="go to home screen" href="/"><span class="mif-home icon"></span></a></li>
-    </ul>
+    <button class="shortcut">
+      <span class="caption">Go Back</span>
+      <span class="mif-arrow-left icon" />
+    </button>
+    <button
+      on:click={() => {
+        nav("/competition");
+      }}
+      class="shortcut"
+    >
+      <span class="caption">Competition</span>
+      <span class="mif-trophy icon" />
+    </button>
+    <button
+      on:click={() => {
+        nav("/category");
+      }}
+      class="shortcut"
+    >
+      <span class="caption">Category</span>
+      <span class="mif-stack  icon" />
+    </button>
+    <button
+      on:click={() => {
+        nav("/entries");
+      }}
+      class="shortcut"
+    >
+      <span class="caption">Entries</span>
+      <span class="mif-users  icon" />
+    </button>
+    <button
+      on:click={() => {
+        nav("/kata");
+      }}
+      class="shortcut"
+    >
+      <span class="caption">Kata</span>
+      <span class="mif-directions-bike  icon" />
+    </button>
+    <button
+      on:click={() => {
+        nav("/pools");
+      }}
+      class="shortcut"
+    >
+      <span class="caption">Pools</span>
+      <span class="mif-apps  icon" />
+    </button>
+    <button
+      on:click={() => {
+        nav("/screen/external");
+      }}
+      class="shortcut"
+    >
+      <span class="caption">External</span>
+      <span class="mif-laptop  icon" />
+    </button>
+    <button
+      on:click={() => {
+        nav("/competition_controller");
+      }}
+      class="shortcut"
+    >
+      <span class="caption">Controller</span>
+      <span class="mif-tree  icon" />
+    </button>
+    <button
+      on:click={() => {
+        nav("/screen/judgescreen");
+      }}
+      class="shortcut"
+    >
+      <span class="caption">Judges</span>
+      <span class="mif-tablet  icon" />
+    </button>
+    <button
+      on:click={() => {
+        nav("/");
+      }}
+      class="shortcut"
+    >
+      <span class="caption">Home</span>
+      <span class="mif-home  icon" />
+    </button>
   </div>
 </div>
+
 <style>
-    .bar{
-        width: 100vw
-    }
+  .bar {
+    width: 100vw;
+  }
 </style>
