19f81bd7ea5e96b5a2b586b5dc76effac8169123
valike23 <ludike23@gmail.com>
2023-07-27 23:52:07 +0100

    Updated the post function to use `findOrCreate`

--- src/routes/api/judges/pool.ts
+++ src/routes/api/judges/pool.ts
@@ -1,6 +1,30 @@
 
 import { poolEntries, poolEntriesJudge } from "../../../Controllers/pools";
 
+// export async function post(req, res) {
+//     try {
+//         let body = JSON.parse(req.fields.judge);
+//         body.status = 1;
+//         let poolEntry = await poolEntries.findOne({ where: { poolId: body.poolId, entryId: body.entryId } });
+//         let updatePoolEntry = await poolEntries.update({ status: 1 }, { where: { id: poolEntry["id"] } });
+//         body.poolEntryId = poolEntry["id"];
+//         console.log(body);
+//         const data: any = await poolEntriesJudge.findOne({ where: { poolEntryId: body.poolEntryId, judgeId: body.judgeId } });
+//         if (data.poolEntryId) {
+//             let poolEntryJudge = await poolEntriesJudge.update(body, { where: { poolEntryId: body.poolEntryId, judgeId: body.judgeId } });
+//             res.json(poolEntryJudge);
+//         }
+//         else {
+//             let poolEntryJudge = await poolEntriesJudge.create(body);
+//             res.json(poolEntryJudge);
+//         }
+
+//     } catch (error) {
+//         console.log(error);
+//         res.status(503).json(error);
+//     }
+// }
+
 export async function post(req, res) {
     try {
         let body = JSON.parse(req.fields.judge);
@@ -9,16 +33,19 @@ export async function post(req, res) {
         let updatePoolEntry = await poolEntries.update({ status: 1 }, { where: { id: poolEntry["id"] } });
         body.poolEntryId = poolEntry["id"];
         console.log(body);
-        const data: any = await poolEntriesJudge.findOne({ where: { poolEntryId: body.poolEntryId, judgeId: body.judgeId } });
-        if (data.poolEntryId) {
-            let poolEntryJudge = await poolEntriesJudge.update(body, { where: { poolEntryId: body.poolEntryId, judgeId: body.judgeId } });
-            res.json(poolEntryJudge);
-        }
-        else {
-            let poolEntryJudge = await poolEntriesJudge.create(body);
-            res.json(poolEntryJudge);
+        
+        // Use `findOrCreate` instead of `findOne` to handle the case when no data is found.
+        const [data, created] = await poolEntriesJudge.findOrCreate({ 
+            where: { poolEntryId: body.poolEntryId, judgeId: body.judgeId },
+            defaults: body // Use `defaults` to create a new entry with the provided data if it doesn't exist.
+        });
+        
+        if (!created) {
+            // If the entry already exists, update it with the new data.
+            await poolEntriesJudge.update(body, { where: { poolEntryId: body.poolEntryId, judgeId: body.judgeId } });
         }
 
+        res.json(data);
     } catch (error) {
         console.log(error);
         res.status(503).json(error);
