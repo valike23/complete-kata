a6229a12a32f3e98664afd8be4aa625c829bd84c
valike23 <ludike23@gmail.com>
2023-07-28 07:17:43 +0100

    Fixed bug: judge score not updated.

--- src/routes/competition_controller.svelte
+++ src/routes/competition_controller.svelte
@@ -35,11 +35,11 @@
   let endofPool = false;
   let handleTimerEnd = () => {
     console.log("timer end");
-    socket.emit("end-timer",{});
+    socket.emit("end-timer", {});
   };
   let isFinal = false;
   let isTeamFinal = false;
-  
+
   let socket = {};
   let result = 0;
   let total = 0;
@@ -70,27 +70,23 @@
             const judgeResp = judgesResult.judges[j];
             if (judgeResp.judgeId == judge.id) {
               judges[index].RESULT = judgeResp.RESULT;
-              if(judges[index].RESULT == 0) judges[index].RESULT = "0";
+              if (judges[index].RESULT == 0) judges[index].RESULT = "0";
               break;
             }
-            judges[index].RESULT =0;
+            judges[index].RESULT = 0;
           }
-        
         }
-        console.log('reset judges: ', judges);
-          setup();
+        console.log("reset judges: ", judges);
+        setup();
       }
     } catch (error) {
       console.log(error);
     }
   };
-const showFinalResult =()=>{
-
-}
-const showRoundResultOnTV =()=>{
-  socket.emit('show-round-tv',{pool})
-
-}
+  const showFinalResult = () => {};
+  const showRoundResultOnTV = () => {
+    socket.emit("show-round-tv", { pool });
+  };
   const startKata = () => {
     socket.emit("start judge", {
       athlete: controller.currentAthlete,
@@ -113,13 +109,13 @@ const showRoundResultOnTV =()=>{
       }
     }
     judges.forEach((j) => {
-      RESULT += j.RESULT;
+      RESULT += Number(j.RESULT);
     });
     total = RESULT;
     if (isComplete) {
       submit = true;
       tempJudges.sort((a, b) => {
-        return a.RESULT - b.RESULT;
+        return Number(a.RESULT) - Number(b.RESULT);
       });
       console.log("tempjudges", tempJudges);
       let lowstResult = tempJudges[0];
@@ -136,12 +132,12 @@ const showRoundResultOnTV =()=>{
       result = RESULT;
     }
   };
-  const showFinalsScore =()=>{
-    console.log('this is the final result release');
+  const showFinalsScore = () => {
+    console.log("this is the final result release");
     socket.emit("final-result", {
       pool: fakePool,
-    })
-  }
+    });
+  };
   const upload = async () => {
     let resp = confirm("do you want to upload the result?");
     if (!resp) return;
@@ -149,16 +145,15 @@ const showRoundResultOnTV =()=>{
       total: result,
     };
     showScoresOnTV = true;
-    if(!isFinal) {socket.emit("result", {
-      athlete: controller.currentAthlete,
-      judges: judges,
-      score: body,
-      pool: fakePool,
-    });}
-    else{
-      socket.emit("reset-timer",{
-
-      })
+    if (!isFinal) {
+      socket.emit("result", {
+        athlete: controller.currentAthlete,
+        judges: judges,
+        score: body,
+        pool: fakePool,
+      });
+    } else {
+      socket.emit("reset-timer", {});
     }
     const form = new FormData();
     form.append("body", JSON.stringify(body));
@@ -215,19 +210,39 @@ const showRoundResultOnTV =()=>{
         {
           caption: "Agree",
           cls: "js-dialog-close alert",
-          onclick: function () {
+          onclick: async function () {
             const id = document.getElementById("test");
             const tec = document.getElementById("tec");
             const judge = document.getElementById("judgeselect");
+            let activeJudge = {};
             judges.forEach((judge2, i) => {
               if (judge2.id == judge.value) {
                 judges[i].RESULT = Number(tec.value);
-               
+                activeJudge = judges[i];
                 judges = judges;
-                return;
               }
+              setup();
+            
+              
             });
-            alert("You clicked Agree action");
+            let judgeResult = {
+          entryId: controller.currentAthlete.id,
+          RESULT: activeJudge.RESULT,
+          judgeId: activeJudge.id,
+          poolId: pool.id,
+        };
+        console.log(judgeResult);
+        let form = new FormData();
+        form.append("judge", JSON.stringify(judgeResult));
+        let data = await (await axios.post(`api/judges/pool`, form)).data;
+        if (data) {
+          await win.Swal.fire({
+            icon: "success",
+            title: "Success",
+            text: "scores uploaded successfully",
+          });
+          
+        }
           },
         },
         {
@@ -241,25 +256,25 @@ const showRoundResultOnTV =()=>{
     });
   };
 
-  const handleTimerStart =()=>{
-    socket.emit("timer-start",{minutes});
-  }
+  const handleTimerStart = () => {
+    socket.emit("timer-start", { minutes });
+  };
   onMount(async () => {
     win = window;
     socket = window.io("/display");
     socket.on("connect", () => {
       console.log(socket.id);
     });
-    console.log('pool', pool);
-    const resp = pool.poolName.split(' ');
+    console.log("pool", pool);
+    const resp = pool.poolName.split(" ");
     //automatically set the app to final if it is a final app
-    if(resp[resp.length - 1] == 'Medal') {
+    if (resp[resp.length - 1] == "Medal") {
       isFinal = true;
-      socket.emit('show-final', {pool});
-      resp.forEach((text)=>{
-        if(text == 'team' || text == 'Team', text == 'TEAM') isTeamFinal = true;
-
-      })
+      socket.emit("show-final", { pool });
+      resp.forEach((text) => {
+        if ((text == "team" || text == "Team", text == "TEAM"))
+          isTeamFinal = true;
+      });
     }
     if (endofPool) {
       handleNotification(
@@ -283,22 +298,20 @@ const showRoundResultOnTV =()=>{
         console.log(error);
         handleNotification(window, "an error occured", EnotificationType.ERROR);
       }
-    };
+    }
     resetVariables();
 
-  
-    socket.on("judge scores",  (data) => {
+    socket.on("judge scores", (data) => {
       console.log(data);
-      if(!data.RESULT){
-        alert(data.RESULT);
-      }
-      else{
+      if (!data.RESULT) {
+        alert('A judge just disqualified an athlete that a look at the board to verify');
+      } else {
         judges.forEach((judge, i) => {
-        if (judge.id == data.judgeId) {
-          judges[i].RESULT = data.RESULT;
-        }
-      });
-      setup();
+          if (judge.id == data.judgeId) {
+            judges[i].RESULT = data.RESULT;
+          }
+        });
+        setup();
       }
     });
   });
@@ -313,23 +326,25 @@ const showRoundResultOnTV =()=>{
 
   <h2>
     Competition Controller {#if isFinal}
+      <button
+        on:click={showFinalsScore}
+        title="use this button to show finals result on TV screen"
+        class="float-right button primary mr-2">Show Final Result</button
+      >
+      <button
+        on:click={showRoundResultOnTV}
+        class=" button primary mr-2"
+        title="show group standing on TV display">show Group Standing</button
+      >
+    {/if}
     <button
-    on:click={showFinalsScore}
-    title="use this button to show finals result on TV screen"
-    class="float-right button primary mr-2">Show Final Result</button
-  >
-  <button
-  on:click={showRoundResultOnTV} 
-  class=" button primary mr-2"
-  title="show group standing on TV display">show Group Standing</button>
-    {/if} <button
       on:click={openJudgeScore}
       title="use this button to upload scores only when judges screen has an issue"
-      class="float-right button primary ">Input Scores</button
+      class="float-right button primary">Input Scores</button
     >
   </h2>
-  <h3> {pool.poolName}</h3>
-  
+  <h3>{pool.poolName}</h3>
+
   <div class="row">
     <div class="cell">
       <div class="card small">
@@ -341,8 +356,14 @@ const showRoundResultOnTV =()=>{
     </div>
     {#if isTeamFinal}
       <div class="cell mb-4">
-        <Timer size="bigger" minutes={minutes} seconds={0} on:timerend={handleTimerEnd} on:timerstart={handleTimerStart} controls={true}  />
-   
+        <Timer
+          size="bigger"
+          {minutes}
+          seconds={0}
+          on:timerend={handleTimerEnd}
+          on:timerstart={handleTimerStart}
+          controls={true}
+        />
       </div>
     {/if}
     <div class="cell float-right">
@@ -377,14 +398,17 @@ const showRoundResultOnTV =()=>{
           ><tr
             ><td class="font-weight-bolder">SCORES</td>{#each judges as judge}
               <td id={"tp" + judge.id}>{judge.RESULT || ""}</td>
-            {/each} <td>{total.toFixed(2)}</td> <td>{result.toFixed(2)}</td></tr
+            {/each}
+            <td>{typeof total == "number" ? total.toFixed(2) : total}</td>
+            <td>{typeof result == "number" ? result.toFixed(2) : result}</td
+            ></tr
           >
           <tr style="height: 75px;">
             {#each judges as judge}
               <td> &nbsp;</td>
             {/each}
             <td style="background-color: red; color: white;"
-              >{result.toFixed(2)}</td
+              >{typeof result == "number" ? result.toFixed(2) : result}</td
             ></tr
           ></tbody
         >
@@ -398,7 +422,6 @@ const showRoundResultOnTV =()=>{
             id="final"
             bind:checked={isFinal}
             data-caption="is Final Bouth"
-            
           /><label for="final">is Team Final Bouth</label>
         </div>
         <div class="cell-9 text-center">
